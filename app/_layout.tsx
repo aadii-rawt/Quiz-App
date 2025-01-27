3e8d46737941212276e6209f7a86219107fb6166
adityarawat4 <rawatadii060@gmail.com>
2025-01-27 15:40:55 +0530

    c

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,7 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/useColorScheme';
-
+import {UserContextProvider} from '@/app/context/useAuthContext'
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
@@ -28,13 +28,18 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <UserContextProvider >
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
         <Stack.Screen name='login' options={{headerShown : false}} />
         <Stack.Screen name='signup' options={{headerShown : false}} />
+        <Stack.Screen name='otpVerification' options={{headerShown : false}} />
+        <Stack.Screen name='home' options={{headerShown : false}} />
       </Stack>
       <StatusBar style="auto" />
+
+      </UserContextProvider>
     </ThemeProvider>
   );
 }
