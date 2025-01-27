9f77173f16277b571ff288d47385288eaab3d994
adityarawat4 <rawatadii060@gmail.com>
2025-01-27 15:41:22 +0530

    c

--- app/signup.jsx
+++ app/signup.jsx
@@ -1,53 +1,83 @@
-import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
-import { Link } from 'expo-router';
+import { Link, useNavigation } from 'expo-router';
+import { auth } from '../firebase';
+import { RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth';
+import OtpVerification from './otpVerification';
 
 const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("")
+  const [confirmation, setConfirmation] = useState(null);
+
+  const navigation = useNavigation()
+  const handleSignup = async () => {
+    try {
+      const recaptchaVerifier = new RecaptchaVerifier(auth, 'recaptcha-container', {
+        'size': 'normal',
+        'callback': (response) => {
+          console.log('Recaptcha verified:', response);
+        },
+      });
+
+      const confirmationResult = await signInWithPhoneNumber(auth, phoneNumber, recaptchaVerifier);
+      setConfirmation(confirmationResult);
+      console.log(confirmationResult);
+
+      Alert.alert('OTP Sent', 'Please check your phone for the OTP.');
+    } catch (error) {
+      Alert.alert('Error', error.message);
+      console.log(error);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Create Account</Text>
-      <Text style={styles.subheading}>Create an account so you can explore all the existing jobs</Text>
+      {!confirmation ? (
+        <>
+          <Text style={styles.heading}>Create Account</Text>
+          <Text style={styles.subheading}>Create an account so you can explore all the existing jobs</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        placeholderTextColor="#B0B0B0"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        placeholderTextColor="#B0B0B0"
-        secureTextEntry
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Confirm Password"
-        placeholderTextColor="#B0B0B0"
-        secureTextEntry
-      />
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            value={username}
+            onChangeText={(text) => setUsername(text)}
+            placeholderTextColor="#B0B0B0"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Phone Number"
+            value={phoneNumber}
+            onChangeText={(text) => setPhoneNumber(text)}
+            placeholderTextColor="#B0B0B0"
+          />
 
-      <TouchableOpacity style={styles.signupButton}>
-        <Text style={styles.signupText}>Sign up</Text>
-      </TouchableOpacity>
+          <View id="recaptcha-container" />
+          <TouchableOpacity onPress={handleSignup} style={styles.signupButton}>
+            <Text style={styles.signupText}>Sign up</Text>
+          </TouchableOpacity>
 
-      <TouchableOpacity>
-        <Link href='/login' style={styles.alreadyAccount}>Already have an account</Link>
-      </TouchableOpacity>
+          <TouchableOpacity>
+            <Link href='/login' style={styles.alreadyAccount}>Already have an account</Link>
+          </TouchableOpacity>
 
-      <Text style={styles.orContinueWith}>Or continue with</Text>
+          <Text style={styles.orContinueWith}>Or continue with</Text>
 
-      <View style={styles.socialIconsContainer}>
-        <TouchableOpacity style={styles.iconButton}>
-          <FontAwesome name="google" size={24} color="black" />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.iconButton}>
-          <FontAwesome name="facebook" size={24} color="black" />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.iconButton}>
-          <FontAwesome name="apple" size={24} color="black" />
-        </TouchableOpacity>
-      </View>
+          <View style={styles.socialIconsContainer}>
+            <TouchableOpacity style={styles.iconButton}>
+              <FontAwesome name="google" size={24} color="black" />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.iconButton}>
+              <FontAwesome name="facebook" size={24} color="black" />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.iconButton}>
+              <FontAwesome name="apple" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+        </>) :
+        <OtpVerification confirmation={confirmation} username={username} phoneNumber={phoneNumber}/>}
     </View>
   );
 };
@@ -100,7 +130,7 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#1D4ED8',
     marginBottom: 20,
-    textDecorationLine : "underline"
+    textDecorationLine: "underline"
   },
   orContinueWith: {
     fontSize: 14,
