a36611e7f105111683bf5f6ddf28e4d6cdf1a0f8
adityarawat4 <rawatadii060@gmail.com>
2025-01-27 15:40:36 +0530

    c

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJe_Pli3afXd1ddVctWbn7tu251_FaMNk",
+  authDomain: "quizapp-fcfb5.firebaseapp.com",
+  projectId: "quizapp-fcfb5",
+  storageBucket: "quizapp-fcfb5.firebasestorage.app",
+  messagingSenderId: "180141145950",
+  appId: "1:180141145950:web:5e6de496aeb0b3a3fad9cf"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const db = getFirestore(app);
+auth.settings.appVerificationDisabledForTesting = true;
