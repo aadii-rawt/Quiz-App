b4882bb2117e7c390eefceb4e274841ac6829360
adityarawat4 <rawatadii060@gmail.com>
2025-02-07 11:50:11 +0530

    commit

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,14 +1,21 @@
 import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import React from 'react'
 import Home from "../home";
+import { SafeAreaView } from 'react-native-safe-area-context';
 const index = () => {
   return (
-    <ScrollView>
-      <Home/>
-    </ScrollView>
+    <SafeAreaView style={styles.safeContainer}>
+      <ScrollView>
+        <Home />
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default index
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1
+  },
+})
